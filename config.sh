332be476fb116d018361372e5badb0d8409a2c5a
elisha-cloudways <82445496+elisha-cloudways@users.noreply.github.com>
2021-11-19 17:13:02 +0500

    Update WP-config -- version 1.0
    Can take table_prefix parameter

---
+++ config.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+db=$1
+pw=$2
+prefix=$3
+
+wp config set DB_NAME $db
+wp config set DB_USER $db
+wp config set DB_PASSWORD $pw
+wp config set DB_HOST localhost
+
+if test -z "$prefix"
+then
+        echo "WP-config updated successfully"
+else
+        wp config set table_prefix $prefix
+        echo "WP-config updated successfully"
+fi
+

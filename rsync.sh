6d7505ce82757fbfedd554b0a3d2e0d6a3ed1d5b
elisha-cloudways <82445496+elisha-cloudways@users.noreply.github.com>
2021-11-20 09:45:28 +0500

    Rsync path attribute fixed

--- rsync.sh
+++ rsync.sh
@@ -3,15 +3,13 @@ db=$1
 user=$2
 ip=$3
 path=$4
-#ssh -i platformops platformops@$ip -p 22
 
 wp db export --no-create-db=true $db.sql
-
-rsync -avuz --progress ../public_html/  $user@$ip://home/master/applications/$db/public_html
 
 if [ -z "$path" ]
 then
         rsync -avuz --progress ../public_html/  $user@$ip://home/master/applications/$db/public_html
+
 else
-      rsync -avuz --progress $path $user@$ip://home/master/applications/$db/public_html
+        rsync -avuz --progress $path $user@$ip://home/master/applications/$db/public_html
 fi

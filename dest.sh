fe58d6416817aef1edf421f71dd3184dd465b1d9
elisha-cloudways <82445496+elisha-cloudways@users.noreply.github.com>
2021-11-23 20:30:40 +0500

    dest.sh 1.0

---
+++ dest.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+db=$1
+pw=$2
+prefix=$3
+
+# Reset Database
+wp db reset --yes
+
+wp config set DB_NAME $db
+wp config set DB_USER $db
+wp config set DB_PASSWORD $pw
+wp config set DB_HOST localhost
+wp config set FS_METHOD direct
+
+if test -z "$prefix"
+then
+        echo $'\n'$(tput setaf 2)WP-config updated successfully$(tput setaf 7) $'\n'
+        else
+                wp config set table_prefix $prefix
+                echo $'\n'$(tput setaf 2)WP-config updated successfully$(tput setaf 7) $'\n'
+        fi
+
+wp config list
+# Import source DB
+wp db import $db.sql
+
+#echo $(tput setaf 2)Database has been imported$'\n'
+# Fetch Source Domain
+srcURL=$(wp db query 'SELECT * FROM wp_options WHERE option_name="siteurl"' --skip-column-names | awk '{print $3}')
+
+destURL="https://$(cat /home/master/applications/$db/conf/server.apache  | grep ServerName |  awk '{print $2}')"
+
+# Print out source and destination URLs before search-replace
+echo $(tput setaf 7) $'\n'Source = $(tput setaf 3)$srcURL $'\n'$(tput setaf 7)Destination = $(tput setaf 3)$destURL $(tput setaf 7)
+#exit;
+
+# Search-replace dry run
+echo $'\n'Search and replace dry run...
+wp search-replace "$srcURL" "https://$destURL" --all-tables --dry-run
+
+get_destURL() {
+       read -p 'Enter Destination URL: ' destURL
+       #echo "$name"
+}
+confirmation(){
+        #echo "Do you wish to proceed?" \ && echo "Source = $srcURL" \ && echo "Destination = $destURL"\ && echo "Enter (1,2,3):"
+        #echo $(tput setaf 7)$'\n'Source = $(tput setaf 3)$srcURL $'\n'$(tput setaf 7)Destination = $(tput setaf 3)$destURL $'\n'Do you wish to proceed? Enter (1,2 or 3):$'\n' 
+        echo $(tput setaf 7)$'\n'Source = $(tput setaf 3)$srcURL $'\n'$(tput setaf 7)Destination = $(tput setaf 3)$destURL $'\n'$(tput setaf 7)$'\n'Do you wish to $(tput setaf 1)proceed$(tput setaf 7)? "Enter (1,2 or 3):"
+        select yn in "Yes" "No" "Re-enter Destination URL"; do
+        case $yn in
+                Yes ) break;;
+                No ) echo $'\n'$(tput setaf 1)Exiting...$(tput setaf 7); exit;;
+                "Re-enter Destination URL" ) get_destURL; confirmation;;
+        esac
+done
+}
+
+confirmation;
+echo $'\n'Running Search and replace...
+wp search-replace "$srcURL" "$destURL" --all-tables
+
+echo "$(tput setaf 2)WP Search and Replace complete. Proceed with  migration checks. Adios!"

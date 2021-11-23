97ee392c715f16b74cb69831a76a153584f9f7f0
elisha-cloudways <82445496+elisha-cloudways@users.noreply.github.com>
2021-11-23 22:57:22 +0500

    rsync.sh updated
    No need to define webroot.
    Also provides option to specify the path, useful incase of transferring a single file.

--- rsync.sh
+++ rsync.sh
@@ -3,12 +3,14 @@ db=$1
 user=$2
 ip=$3
 path=$4
+webroot=$(pwd)/
 
+echo $'\n'Exporting database...
 wp db export --no-create-db=true $db.sql
 
 if [ -z "$path" ]
 then
-        rsync -avuz --progress ../public_html/  $user@$ip://home/master/applications/$db/public_html
+        rsync -avuz --progress $webroot $user@$ip://home/master/applications/$db/public_html
 
 else
         rsync -avuz --progress $path $user@$ip://home/master/applications/$db/public_html

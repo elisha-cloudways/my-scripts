12a0f0b9da674ecde36b248695fa0c4b13cd2c52
elisha-cloudways <82445496+elisha-cloudways@users.noreply.github.com>
2021-11-24 16:49:16 +0500

    destination.sh
    Includes MySQL connection test.

---
+++ destination.sh
@@ -0,0 +1,105 @@
+#!/bin/bash
+# db=$1
+# pw=$2
+# prefix=$3
+tab_prefix=$(<prefix.txt);
+dbaccess="denied"
+
+get_db(){
+        read -p 'Destination DB name: ' db
+}
+
+get_pw(){
+        read -p 'Destination DB password: ' pw
+}
+get_db;
+get_pw;
+
+until [[ $dbaccess = "success" ]]; do
+        echo "Verifying MySQL credentials..."
+        mysql --user="${db}" --password="${pw}" -e exit 2>/dev/null
+        dbstatus=`echo $?`
+        if [ $dbstatus -ne 0 ]; then
+                #echo -e "Destination MySQL Username [$db]: \c "
+                #read dbuser
+                echo $(tput setaf 1)Failed: $(tput setaf 7)Incorrect database credentials $'\n'
+                get_db;
+                get_pw;
+        else
+                dbaccess="success"
+                #echo "Success!"
+                echo $(tput setaf 2)Success: $(tput setaf 7)Database credentials verified $'\n'
+        fi
+done
+
+echo "Updating WP-config details..."
+wp config set DB_NAME $db
+wp config set DB_USER $db
+wp config set DB_PASSWORD $pw
+wp config set table_prefix $tab_prefix
+wp config set DB_HOST localhost
+wp config set FS_METHOD direct
+
+echo $'\n'$(tput setaf 2)Success: $(tput setaf 7)WP-config updated successfully$(tput setaf 7) $'\n':::::
+#if test -z "$prefix"
+#then
+#        echo $'\n'$(tput setaf 2)Success: $(tput setaf 7)WP-config updated successfully$(tput setaf 7) $'\n'
+#        else
+#                wp config set table_prefix $prefix
+#                echo $'\n'$(tput setaf 2)Success: $(tput setaf 7)WP-config updated successfully$(tput setaf 7) $'\n'
+#        fi
+
+wp config list
+
+# Reset Database
+echo $'\n'Resetting existing database...
+wp db reset --yes
+
+# Import source DB
+echo $'\n'Importing source MySQL file...
+wp db import $db.sql
+
+#echo $(tput setaf 2)Database has been imported$'\n'
+# Fetch Source Domain
+srcURL=$(wp db query 'SELECT * FROM wp_options WHERE option_name="siteurl"' --skip-column-names | awk '{print $3}')
+
+destURL="https://$(cat /home/master/applications/$db/conf/server.apache  | grep ServerName |  awk '{print $2}')"
+
+# Print out source and destination URLs before search-replace
+echo $(tput setaf 7) $'\n'Source = $(tput setaf 3)$srcURL $'\n'$(tput setaf 7)Destination = $(tput setaf 3)$destURL $(tput setaf 7)
+#exit;
+
+# Search-replace dry run
+echo $'\n'Search and replace dry run...
+wp search-replace "$srcURL" "https://$destURL" --all-tables --dry-run
+
+get_destURL() {
+       read -p 'Enter Destination URL: ' destURL
+       #echo "$name"
+}
+confirmation(){
+        #echo "Do you wish to proceed?" \ && echo "Source = $srcURL" \ && echo "Destination = $destURL"\ && echo "Enter (1,2,3):"
+        #echo $(tput setaf 7)$'\n'Source = $(tput setaf 3)$srcURL $'\n'$(tput setaf 7)Destination = $(tput setaf 3)$destURL $'\n'Do you wish to proceed? Enter (1,2 or 3):$'\n' 
+        echo $(tput setaf 7)$'\n'Source = $(tput setaf 3)$srcURL $'\n'$(tput setaf 7)Destination = $(tput setaf 3)$destURL $'\n'$(tput setaf 7)$'\n'Do you wish to $(tput setaf 1)proceed$(tput setaf 7)? "Enter (1,2 or 3):"
+        select yn in "Yes" "No" "Re-enter Destination URL"; do
+        case $yn in
+                Yes ) break;;
+                No ) echo $'\n'$(tput setaf 1)Exiting...$(tput setaf 7); exit;;
+                "Re-enter Destination URL" ) get_destURL; confirmation;;
+        esac
+done
+}
+
+confirmation;
+echo $'\n'Running search and replace...
+wp search-replace "$srcURL" "$destURL" --all-tables
+echo "$(tput setaf 2)Success: $(tput setaf 7)WP Search and Replace complete."
+
+echo $'\n'Removing WP cache...
+wp cache flush
+echo "$(tput setaf 7)Removing cache content in wp-content/cache..."
+rm -rf /home/master/applications/$db/public_html/wp-content/cache/*
+
+echo "$(tput setaf 2)Success: $(tput setaf 7)Cache deleted in wp-content/cache."
+echo "Proceed with migration checks. Adios!"
+exit;

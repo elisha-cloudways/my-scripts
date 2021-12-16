333d4418ee0b78a49c40fdba0c8a9a9bc0d6c4b7
elisha-cloudways <82445496+elisha-cloudways@users.noreply.github.com>
2021-12-16 11:36:48 +0500

    Investigate CPU load

---
+++ apm.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+cd /home/master/applications/
+for A in $(ls -l /home/master/applications/| grep "^d" | awk '{print $NF}'); do echo $A && awk '{print $1,$7}' /home/master/applications/$A/logs/apache_*.access.log | cut -d? -f1 | sort | uniq -c |sort -nr | head -n 5 | awk -F";" '{print $1}' ; done
+
+for A in $(ls | awk '{print $NF}'); do echo $A && sudo apm traffic -s $A -l 1d; done
+for A in $(ls | awk '{print $NF}'); do echo $A && sudo apm mysql -s $A -l 1d; done
+for A in $(ls | awk '{print $NF}'); do echo $A && sudo apm php -s $A -l 1d; done

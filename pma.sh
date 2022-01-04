e2f8082081f71c67a86f39d3d828f36c33433d57
elisha-cloudways <82445496+elisha-cloudways@users.noreply.github.com>
2022-01-04 16:45:13 +0500

    Update pma.sh
    Removing script

--- pma.sh
+++ pma.sh
@@ -1,3 +1,7 @@
+#!/bin/bash
+
 wget https://files.phpmyadmin.net/phpMyAdmin/5.1.1/phpMyAdmin-5.1.1-english.zip
 unzip phpMyAdmin-5.1.1-english.zip 
 mv phpMyAdmin-5.1.1-english phpmyadmin
+rm phpMyAdmin-5.1.1-english.zip
+rm pma.sh

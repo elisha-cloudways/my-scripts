082f10b672e21443a6412bc9359500b0ea34604a
elisha-cloudways <82445496+elisha-cloudways@users.noreply.github.com>
2022-01-04 22:38:00 +0500

    Update chkdu.sh
    Send permission errors to /dev/null

--- chkdu.sh
+++ chkdu.sh
@@ -7,7 +7,7 @@ do
         echo $'\n'$d
         sudo apm -s $d -d
         echo App folder taking most space: $'\n'
-        du -h -d2 $d* | sort -hr | head -n 4
+        du -h -d2 $d* 2>/dev/null | sort -hr | head -n 4
 done
 cd
 rm ./chkdu.sh

c038507f10f85c109dc324c0f714199211735a8b
elisha-cloudways <82445496+elisha-cloudways@users.noreply.github.com>
2022-01-05 17:00:07 +0500

    Update chkdu.sh

--- chkdu.sh
+++ chkdu.sh
@@ -2,12 +2,12 @@
 
 echo -e "\nDisk usage:"; df -h /dev/vda1; echo $'\n'; du -shc /*  2>/dev/null |sort -rh | head -n 6
 cd /home/master/applications/
-for d in ./*/
+for d in ./*
 do
         echo $'\n'$d
         sudo apm -s $d -d
         echo App folder taking most space: $'\n'
-        du -h -d2 $d* 2>/dev/null | sort -hr | head -n 4
+        du -h -d2 $d/* 2>/dev/null | sort -hr | head -n 4
 done
 cd
 rm ./chkdu.sh

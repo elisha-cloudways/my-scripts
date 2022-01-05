327327149bbe63235becc4ca3b332379b52e5892
elisha-cloudways <82445496+elisha-cloudways@users.noreply.github.com>
2022-01-05 17:04:16 +0500

    Update chkdu.sh
    Updated $d

--- chkdu.sh
+++ chkdu.sh
@@ -2,7 +2,7 @@
 
 echo -e "\nDisk usage:"; df -h /dev/vda1; echo $'\n'; du -shc /*  2>/dev/null |sort -rh | head -n 6
 cd /home/master/applications/
-for d in ./*
+for d in *
 do
         echo $'\n'$d
         sudo apm -s $d -d

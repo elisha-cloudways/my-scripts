83122e37c5cb06911c68dfcfa8a4372d0455b90c
elisha-cloudways <82445496+elisha-cloudways@users.noreply.github.com>
2022-01-17 23:12:00 +0500

    Update cpu-apm.sh

--- cpu-apm.sh
+++ cpu-apm.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo $(tput setaf 3)Application logs$(tput setaf 7);
+echo Fetching logs to debug $(tput setaf 1)Server Load$(tput setaf 7);
 cd /home/master/applications/
 # for A in $(ls -l /home/master/applications/| grep "^d" | awk '{print $NF}'); do echo $A && awk '{print $1,$7}' /home/master/applications/$A/logs/apache_*.access.log | cut -d? -f1 | sort | uniq -c |sort -nr | head -n 5 | awk -F";" '{print $1}' ; done
 for A in $(ls | awk '{print $NF}'); do echo $'\n'$(tput setaf 4)$A $(tput setaf 7); cat $A/conf/server.nginx | awk '{print $NF}' | head -n1 && sudo apm traffic -s $A -l 1d; sudo apm mysql -s $A -l 1d; sudo apm php -s $A --slow_pages -l 1d; done

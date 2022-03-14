9aaae27aa4c2c91406949b7892d5dc78f580122f
elisha-cloudways <82445496+elisha-cloudways@users.noreply.github.com>
2022-03-14 18:25:54 +0500

    Update apm-stats.sh
    Updated Year field to %Y.
    Removed apm cron stats

--- apm-stats.sh
+++ apm-stats.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # Purpose: Get server stats for a certain duration
 # Author: Elisha | Cloudways
+# Last Edited: 14/03/2022:18:23
 set -e
 date_to_check=$1
 time_in_UTC=$2
@@ -12,13 +13,13 @@ yy=$(echo $date_to_check | cut -d '/' -f3);
 
 # Convert format 
 date_new="$mm/$dd/$yy"
-
-from_param=$(date --date="$date_new $time_in_UTC UTC -$interval_in_mins min" -u +'%d/%m/%y:%H:%M');
-
-until_param=$(date --date="$date_new $time_in_UTC UTC +$interval_in_mins min" -u +'%d/%m/%y:%H:%M');
-
-cd /home/master/applications/
-for A in $(ls | awk '{print $NF}'); do echo $'\n'$(tput setaf 4)$A $(tput setaf 7); cat $A/conf/server.nginx | awk '{print $NF}' | head -n1 && sudo apm -s $A traffic --from $from_param --until $until_param; sudo apm -s $A mysql --from $from_param --until $until_param; sudo apm -s $A php --slow_pages --from $from_param --until $until_param; sudo apm -s $A cron --from $from_param --until $until_param; done
-cd - && rm apm-stats.sh;
+#echo $date_new;
 #exit;
 
+from_param=$(date --date="$date_new $time_in_UTC UTC -$interval_in_mins min" -u +'%d/%m/%Y:%H:%M');
+until_param=$(date --date="$date_new $time_in_UTC UTC +$interval_in_mins min" -u +'%d/%m/%Y:%H:%M');
+
+cd /home/master/applications/
+for A in $(ls | awk '{print $NF}'); do echo $'\n'$(tput setaf 4)$A $(tput setaf 7); cat $A/conf/server.nginx | awk '{print $NF}' | head -n1 && sudo apm -s $A traffic --from $from_param --until $until_param; sudo apm -s $A mysql --from $from_param --until $until_param; sudo apm -s $A php --slow_pages --from $from_param --until $until_param; done
+#cd - && rm apm-stats.sh;
+#exit;

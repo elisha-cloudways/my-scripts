a58792425bed4022b7cf6dabc6e8979ac2e1b0e5
elisha-cloudways <82445496+elisha-cloudways@users.noreply.github.com>
2022-05-13 19:47:52 +0500

    Update node_update.sh

--- node_update.sh
+++ node_update.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 cd /home/master
 curl https://gist.githubusercontent.com/cloudways-haider/f7cb6627f6674c263624589d360e12b4/raw/9d0b2c78ace5b7b2dedb411e9d676129e34b470a/nvm_install.sh | bash
-source ~/.bashrc
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 nvm install node
 cd -
 rm ~/node_update.sh

03dae273e2f380a120d7d4ae04707671a4bd43e2
elisha-cloudways <82445496+elisha-cloudways@users.noreply.github.com>
2022-05-19 21:15:45 +0500

    Get high server load stats for top 5 applications

---
+++ apm.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+# Purpose: Debug server load
+# Author: Elisha | Cloudways
+# Last Edited: 18/03/2022:15:30
+
+set -e
+cd /home/master/applications/
+date_to_check=$1
+time_in_UTC=$2
+interval_in_mins=$3
+
+# Adding option to select interval (days/hr/mins)
+iv=$4
+
+# Function to fetch stats for a particular time duration
+get_stats(){
+dd=$(echo $date_to_check | cut -d '/' -f1);
+mm=$(echo $date_to_check | cut -d '/' -f2);
+yy=$(echo $date_to_check | cut -d '/' -f3);
+
+# Convert input date to m/d/y format 
+date_new="$mm/$dd/$yy"
+
+time_a="$date_to_check:$time_in_UTC"
+time_b=$(date --date="$date_new $time_in_UTC UTC $interval_in_mins $iv" -u +'%d/%m/%Y:%H:%M');
+time_op=$(echo $interval_in_mins |  cut -c1-1);
+
+if [[ "$time_op" = "-" ]];
+then
+        from_param=$time_b;
+        until_param=$time_a;
+elif [[ "$time_op" = "+" ]];
+then
+        from_param=$time_a;
+        until_param=$time_b;
+fi
+
+echo Stats from $(tput setaf 3)$from_param $(tput setaf 7)to $(tput setaf 3)$until_param $(tput setaf 7)$'\n'
+top_five=$(for i in $(ls -l | grep -v ^l | awk '{print $NF}' | awk 'FNR > 1'); do count=$(sudo apm -s $i traffic --statuses --from $from_param --until $until_param -j | grep -Po "\d..\",\d*" | cut -d ',' -f2 | head -n1); echo $i:$count ; done | sort -k2 -nr -t ":" | cut -d : -f 1 | head -n 5) ; done
+for A in $top_five; do echo $'\n'$(tput setaf 4)$A $(tput setaf 7); cat $A/conf/server.nginx | awk '{print $NF}' | head -n1 && sudo apm -s $A traffic --from $from_param --until $until_param; sudo apm -s $A mysql --from $from_param --until $until_param; sudo apm -s $A php --slow_pages --from $from_param --until $until_param; done
+}
+
+
+if [ -z $date_to_check ] && [ -z $time_in_UTC ] && [ -z $interval_in_mins ] && [ -z $iv ];
+  then
+    read -p 'Enter duration: ' dur
+    echo "Fetching logs for the last$(tput setaf 1) $dur$(tput setaf 7) ...";
+    top_five=$(for i in $(ls -l | grep -v ^l | awk '{print $NF}' | awk 'FNR > 1'); do count=$(sudo apm -s $i traffic --statuses $dur -j | grep -Po "\d..\",\d*" | cut -d ',' -f2 | head -n1); echo $i:$count ; done | sort -k2 -nr -t ":" | cut -d : -f 1 | head -n 5) ; done
+    for A in $top_five; do echo $'\n'$(tput setaf 4)$A $(tput setaf 7); cat $A/conf/server.nginx | awk '{print $NF}' | head -n1 && sudo apm traffic -s $A -l $dur; sudo apm mysql -s $A -l $dur; sudo apm php -s $A --slow_pages -l $dur; done;
+
+elif [ -z $iv ]
+  then
+    # Setting interval to mins if empty
+    iv="min";
+    get_stats;
+  else
+    get_stats;
+fi;
+
+# cd - && rm apm-stats.sh;
+exit;

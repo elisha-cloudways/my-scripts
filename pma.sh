f634ae406ffa5e70b7867c8e0203520f66e5252a
elisha-cloudways <82445496+elisha-cloudways@users.noreply.github.com>
2022-08-03 18:17:40 +0500

    Update pma.sh
    removed code to remove file.
    Use curl instead.

--- pma.sh
+++ pma.sh
@@ -5,4 +5,3 @@ wget https://files.phpmyadmin.net/phpMyAdmin/5.2.0/phpMyAdmin-5.2.0-english.zip
 unzip phpMyAdmin-5.2.0-english.zip
 mv phpMyAdmin-5.2.0-english phpmyadmin
 rm -rf phpMyAdmin-5.2.0-english.zip
-rm pma.sh

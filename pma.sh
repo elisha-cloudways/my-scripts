9fbc34caea4b1c88c921c17bb71c7abc20d3d067
elisha-cloudways <82445496+elisha-cloudways@users.noreply.github.com>
2022-08-03 17:49:47 +0500

    Update pma.sh

--- pma.sh
+++ pma.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Install PHPMyadmin v5.2.0
 
-wget https://files.phpmyadmin.net/phpMyAdmin/5.1.3/phpMyAdmin-5.2.0-english.zip
+wget https://files.phpmyadmin.net/phpMyAdmin/5.2.0/phpMyAdmin-5.2.0-english.zip
 unzip phpMyAdmin-5.2.0-english.zip
 mv phpMyAdmin-5.2.0-english phpmyadmin
 rm -rf phpMyAdmin-5.2.0-english.zip
